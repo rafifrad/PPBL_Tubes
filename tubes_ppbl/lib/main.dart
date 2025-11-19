901240219573af367325a1f591c0a13d979fbaac
rafifrad <rafifradhitya@gmail.com>
2025-11-19 22:07:51 +0700

    merubah listview menjadi drawer

--- tubes_ppbl/lib/main.dart
+++ tubes_ppbl/lib/main.dart
@@ -34,54 +34,25 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  late Widget _currentScreen;
-  late String _currentTitle;
-  final List<_DrawerItem> _menuItems = [
-    _DrawerItem(
-      title: 'Beranda',
-      icon: Icons.dashboard_outlined,
-      builder: () => const HomeScreen(),
-    ),
-    _DrawerItem(
-      title: 'Persediaan Makanan',
-      icon: Icons.fastfood_outlined,
-      builder: () => const FoodScreen(),
-    ),
-    _DrawerItem(
-      title: 'Peralatan Kamar',
-      icon: Icons.chair_alt_outlined,
-      builder: () => const EquipmentScreen(),
-    ),
-    _DrawerItem(
-      title: 'Laundry',
-      icon: Icons.local_laundry_service_outlined,
-      builder: () => const LaundryScreen(),
-    ),
-    _DrawerItem(title: 'CRUD 5', icon: Icons.build_outlined),
-    _DrawerItem(title: 'CRUD 6', icon: Icons.storage_outlined),
-    _DrawerItem(title: 'CRUD 7', icon: Icons.task_outlined),
-    _DrawerItem(title: 'CRUD 8', icon: Icons.inventory_2_outlined),
-    _DrawerItem(title: 'CRUD 9', icon: Icons.list_alt_outlined),
+  int _currentIndex = 0;
+  
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const FoodScreen(),
+    const EquipmentScreen(),
+    const LaundryScreen(),
   ];
 
-  @override
-  void initState() {
-    super.initState();
-    _currentScreen = const HomeScreen();
-    _currentTitle = 'Beranda';
-  }
+  final List<String> _titles = [
+    'Home',
+    'Persediaan Makanan', 
+    'Peralatan Kamar',
+    'Laundry'
+  ];
 
-  void _selectMenu(_DrawerItem item) {
-    Navigator.pop(context);
-    if (item.builder == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('${item.title} belum tersedia')),
-      );
-      return;
-    }
+  void _onItemTapped(int index) {
     setState(() {
-      _currentScreen = item.builder!();
-      _currentTitle = item.title;
+      _currentIndex = index;
     });
   }
 
@@ -89,7 +60,7 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_currentTitle, style: const TextStyle(color: Colors.black)),
+        title: Text(_titles[_currentIndex]),
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
         elevation: 0,
@@ -117,25 +88,30 @@ class _MainScreenState extends State<MainScreen> {
                 ],
               ),
             ),
-            ..._menuItems.map(
-              (item) => ListTile(
-                leading: Icon(item.icon),
-                title: Text(item.title),
-                onTap: () => _selectMenu(item),
+            ..._titles.asMap().entries.map(
+              (entry) => ListTile(
+                leading: _getIcon(entry.key),
+                title: Text(entry.value),
+                onTap: () {
+                  Navigator.pop(context);
+                  _onItemTapped(entry.key);
+                },
               ),
             ),
           ],
         ),
       ),
-      body: _currentScreen,
+      body: _screens[_currentIndex],
     );
   }
-}
 
-class _DrawerItem {
-  final String title;
-  final IconData icon;
-  final Widget Function()? builder;
-
-  const _DrawerItem({required this.title, required this.icon, this.builder});
+  Icon _getIcon(int index) {
+    switch (index) {
+      case 0: return const Icon(Icons.home_outlined);
+      case 1: return const Icon(Icons.fastfood_outlined);
+      case 2: return const Icon(Icons.chair_alt_outlined);
+      case 3: return const Icon(Icons.local_laundry_service_outlined);
+      default: return const Icon(Icons.error);
+    }
+  }
 }
